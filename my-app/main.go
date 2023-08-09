4f7a9d16b134a8fb117b732b38a33becd3c4eb54
DPontaro <n.douzono@gmail.com>
2023-08-09 23:07:13 +0900

    iotaの学び

--- my-app/main.go
+++ my-app/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+// iotaは都度評価される
+// Appleは 0 + 0
+// Orangeは 1 + 5
+// Bananaは 2 + 10
+const (
+	Apple = iota + iota
+	Orange = iota + 5
+	Banana = iota + iota + 10
+)
+
 func main() {
+	fmt.Println(Apple)
+	fmt.Println(Orange)
+	fmt.Println(Banana)
+
 	fmt.Println("Hello, World!")
 }
